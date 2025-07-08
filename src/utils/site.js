bc119034fecbdbd9f655e2688e9c020f69eb906a
furnitureeigo <furnitureeigo@gmail.com>
2025-07-08 08:02:04 +0700

    Update site.js

--- src/utils/site.js
+++ src/utils/site.js
@@ -5,3 +5,5 @@ export const terbit = '2025-03-09T00:01:03Z';
 
 export const google = '6PhY1qnztta4gyFWqvqp0bM_dFfSux202SBKeT-VoWE';
 export const yandex = '41bd8e8ad3ba318a';
+
+export const index = '8c31a2dd-6200-4c62-b0c0-9cb30d5b50f5';
